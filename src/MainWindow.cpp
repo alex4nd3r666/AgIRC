97c8ae18cdef37ec8f52fc39152c11a4071450aa
CALLlA1011666 <lexa2268spb@gmail.com>
2025-06-29 21:45:34 +0300

    29062025-UI003

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -21,17 +21,75 @@ using namespace declarative;
 
 MainWindow::MainWindow(_<MyUpdater> updater)
   : AWindow("Project template app", 640_dp, 480_dp), mUpdater(std::move(updater)) {
-    AStylesheet::global().addRules({ {
-      t<AView>(), FontFamily { "AcPlus_IBM_VGA_8x16-2x" }
+  AStylesheet::global().addRules({ {
+      t<AView>(),
+      Font { ":fonts/AcPlus_IBM_VGA_8x16-2x.ttf" },
       // BackgroundSolid { 0x000_rgb },
+      Margin(0), Expanding()
 
-    } }),
-        setContents(Vertical {
-          Horizontal {} with_style { BackgroundSolid { 0x000_rgb }, FixedSize({}, 20_dp) }, 
-          Horizontal { 
-                Vertical { SpacerExpanding() } with_style {
-              BackgroundSolid { 0xff0000_rgb }, MinSize(48_dp, 320_dp), Expanding() }, 
-                Vertical {},
-            }
-        });
+    }
+  }),
+  setContents(Vertical {
+          Horizontal { Label { "ARGENTUM INTERNET RELAY CHAT" } with_style { TextColor { 0xff0000_rgb } } } with_style {
+            BackgroundSolid { 0x000_rgb }, FixedSize({}, 20_dp) },
+    Horizontal {
+      Horizontal { Vertical::Expanding {
+          Horizontal {
+            Vertical::Expanding { SpacerExpanding() } with_style {
+              BackgroundSolid { 0xff0000_rgb },
+              MaxSize(48_dp, {}),
+              MinSize(48_dp, 320_dp),
+              Expanding()
+            },
+                Vertical::Expanding { SpacerExpanding() } with_style {
+              BackgroundSolid { 0xff00_rgb },
+              MaxSize(96_dp, {}),
+              MinSize(96_dp, 320_dp),
+              Expanding() },
+          },
+          Vertical {
+            Label { "PRAISE THE CODE" } 
+          } with_style {
+                BackgroundSolid { 0xffff_rgb }, FixedSize(144_dp, 64_dp) },
+        }
+      },
+      Horizontal {
+        Vertical {
+          Horizontal {
+                  Vertical::Expanding { SpacerExpanding() } with_style {
+              BackgroundSolid { 0xff_rgb },
+              MinSize(320_dp, 320_dp),
+              Expanding() },
+
+          },
+          Vertical {
+            Label { "NEL NOME DEL CODICE" } 
+          } with_style {
+                  BackgroundSolid { 0xff00ff_rgb }, FixedSize({}, 32_dp), Expanding() },
+        },
+
+      },
+      
+      Horizontal {
+        Vertical {
+          Horizontal {
+                  Vertical::Expanding { SpacerExpanding() } with_style {
+              BackgroundSolid { 0xff0000_rgb }, 
+              MinSize({} ,320_dp),
+              FixedSize(144_dp, {}),
+              Expanding() 
+      },
+
+          },
+          Vertical {
+            Label { "LODE AL DISCO SACRO" } ,
+                } with_style { BackgroundSolid { 0xffff00_rgb },
+
+                               FixedSize(144_dp, 64_dp), Expanding() }
+      },
+
+            } } with_style { Expanding() },
+          Horizontal {} with_style { BackgroundSolid { 0x000_rgb }, FixedSize({}, 20_dp) },
+  }
+             );
 }
